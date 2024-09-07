9e232caa434d69421693006eb8ddc6ce1fd88cd2
Matt Ortner <matthewortner@gmail.com>
2024-09-07 14:33:23 -0400

    create date functions

---
+++ src/currentDateString.js
@@ -0,0 +1,7 @@
+const today = new Date();
+const day = String(today.getDate()).padStart(2, "0");
+const month = String(today.getMonth() + 1).padStart(2, "0");
+const year = today.getFullYear();
+const dateAsString = `${year}-${month}-${day}`;
+
+export default dateAsString;
