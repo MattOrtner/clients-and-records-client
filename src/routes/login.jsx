a72c8eabc25a9b8b26a1b485a423d2fa1f9cc203
Matt Ortner <matthewortner@gmail.com>
2024-10-25 13:45:17 -0400

    style/ add background gradient for login

--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -4,7 +4,7 @@ export default function Login({ handleInput, pass, email, handleSubmit }) {
   return (
     <div className="w-screen h-screen flex justify-center items-center">
       <div className=" h-3/4 w-3/4 flex flex-col justify-center items-center gap-8">
-        <h1 className="text-3xl text-center font-serif w-full">Login</h1>
+        <h1 className="text-4xl font-serif w-full">Login</h1>
         <Form method="POST" className="flex flex-col gap-4 self-end w-full ">
           <input
             type="text"
