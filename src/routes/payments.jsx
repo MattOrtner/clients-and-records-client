cf7339248083c44da4206ece4cd63a128b3fd303
Matt Ortner <matthewortner@gmail.com>
2025-04-07 14:53:45 -0400

    style: add gap between payment session items for improved layout

--- src/routes/payments.jsx
+++ src/routes/payments.jsx
@@ -23,14 +23,14 @@ const Payments = () => {
       <h1 className="text-4xl flex justify-between text-gray-800 font-serif mb-10">
         Unpaid Sessions
       </h1>
-      <div className="flex flex-col h-[85%] overflow-scroll">
+      <div className="flex flex-col h-[85%] gap-2 overflow-scroll">
         {sessions.length > 0 ? (
           sessions.map((session) => (
             <Link
               to={`/${user.id}/clients/${session.client_id}`}
               key={session.session_id}
             >
-              <div className="flex flex-col gap-2 mb-4 p-4 bg-white rounded-lg shadow-md border border-slate-200">
+              <div className="flex flex-col gap-2 p-4 bg-white rounded-lg shadow-md border border-slate-200">
                 <h2 className="text-lg font-bold">{`${session.first} ${session.last}`}</h2>
                 <div className="flex justify-between px-2">
                   <h2 className="text-lg">{session.date}</h2>
