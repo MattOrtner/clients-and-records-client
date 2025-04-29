61e522bb8638d7bbfca544b6e0d63b458c05eb2a
Matt Ortner <matthewortner@gmail.com>
2025-04-29 15:10:24 -0400

    style: move add contact button to bottom of the contactList

--- src/routes/contactList.jsx
+++ src/routes/contactList.jsx
@@ -28,32 +28,37 @@ export default function ContactList() {
       <h1 className="text-4xl text-gray-800 font-serif p-8">
         Happy {CurrentDay} {user.first}!
       </h1>
-      <div className="flex w-full justify-end">
-        <Form method="post">
-          <button className=" text-white my-4 mx-4 rounded-full" type="submit">
-            <Icon path={mdiPlus} color="rgb(59 130 246)" size={1.5} />
-          </button>
-        </Form>
-      </div>
       <nav className="w-full flex flex-col items-center">
         {clients.length > 0 ? (
-          <ul className="w-full p-2 flex flex-col gap-1 items-center overflow-scroll no-scrollbar">
-            {clients.map((client) => (
-              <NavLink
-                key={client.id}
-                to={`/${user.id}/clients/${client.id}`}
-                className="flex justify-center items-center w-full border-gray-300 border rounded-lg py-2"
-              >
-                {client.first || client.last ? (
-                  <div className="text-xl w-[90%] text-center">
-                    {client.first} {client.last}
-                  </div>
-                ) : (
-                  <i>No Name</i>
-                )}
-              </NavLink>
-            ))}
-          </ul>
+          <>
+            <ul className="w-full p-2 flex flex-col gap-1 items-center overflow-scroll no-scrollbar">
+              {clients.map((client) => (
+                <NavLink
+                  key={client.id}
+                  to={`/${user.id}/clients/${client.id}`}
+                  className="flex justify-center items-center w-full border-gray-300 border rounded-lg py-2"
+                >
+                  {client.first || client.last ? (
+                    <div className="text-xl w-[90%] text-center">
+                      {client.first} {client.last}
+                    </div>
+                  ) : (
+                    <i>No Name</i>
+                  )}
+                </NavLink>
+              ))}
+            </ul>
+            <div className="fixed bottom-24 right-4 flex w-full justify-end">
+              <Form method="post">
+                <button
+                  className=" text-white my-4 mx-4 rounded-full"
+                  type="submit"
+                >
+                  <Icon path={mdiPlus} color="rgb(59 130 246)" size={2} />
+                </button>
+              </Form>
+            </div>
+          </>
         ) : (
           <p className="text-2xl mt-28">Add a contact</p>
         )}
