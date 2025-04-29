a1884f3b5e56cf07a8cd1f02a3d7a1365166c4ca
Matt Ortner <matthewortner@gmail.com>
2025-04-28 21:54:02 -0400

    refactor: remove outer fragment, change addContact button to match addSession button, limit with of whole list to half of the screen

--- src/routes/contactList.jsx
+++ src/routes/contactList.jsx
@@ -9,6 +9,7 @@ import {
 import Icon from "@mdi/react";
 import { mdiPlus } from "@mdi/js";
 import { getContacts } from "../contacts";
+import CurrentDay from "../currentDay";
 
 export async function action(req) {
   return redirect(`/${req.params.userId}/clients/create`);
@@ -23,39 +24,40 @@ export default function ContactList() {
   const clients = useLoaderData();
 
   return (
-    <>
-      <div className="w-full">
-        <span className="flex w-full justify-end p-2">
-          <Form method="post">
-            <button className="bg-blue-500 text-white my-8 mx-4" type="submit">
-              <Icon path={mdiPlus} size={1} />
-            </button>
-          </Form>
-        </span>
-        <nav className="w-full flex flex-col items-center">
-          {clients.length > 0 ? (
-            <ul className="w-full p-2 flex flex-col gap-1 items-center overflow-scroll no-scrollbar">
-              {clients.map((client) => (
-                <NavLink
-                  key={client.id}
-                  to={`/${user.id}/clients/${client.id}`}
-                  className="flex justify-center items-center w-full border-gray-300 border rounded-lg py-2"
-                >
-                  {client.first || client.last ? (
-                    <div className="text-xl w-[90%] text-center">
-                      {client.first} {client.last}
-                    </div>
-                  ) : (
-                    <i>No Name</i>
-                  )}
-                </NavLink>
-              ))}
-            </ul>
-          ) : (
-            <p className="text-2xl mt-28">Add a contact</p>
-          )}
-        </nav>
+    <div className="w-full sm:w-1/2">
+      <h1 className="text-4xl text-gray-800 font-serif p-8">
+        Happy {CurrentDay} {user.first}!
+      </h1>
+      <div className="flex w-full justify-end">
+        <Form method="post">
+          <button className=" text-white my-4 mx-4 rounded-full" type="submit">
+            <Icon path={mdiPlus} color="rgb(59 130 246)" size={1.5} />
+          </button>
+        </Form>
       </div>
-    </>
+      <nav className="w-full flex flex-col items-center">
+        {clients.length > 0 ? (
+          <ul className="w-full p-2 flex flex-col gap-1 items-center overflow-scroll no-scrollbar">
+            {clients.map((client) => (
+              <NavLink
+                key={client.id}
+                to={`/${user.id}/clients/${client.id}`}
+                className="flex justify-center items-center w-full border-gray-300 border rounded-lg py-2"
+              >
+                {client.first || client.last ? (
+                  <div className="text-xl w-[90%] text-center">
+                    {client.first} {client.last}
+                  </div>
+                ) : (
+                  <i>No Name</i>
+                )}
+              </NavLink>
+            ))}
+          </ul>
+        ) : (
+          <p className="text-2xl mt-28">Add a contact</p>
+        )}
+      </nav>
+    </div>
   );
 }
